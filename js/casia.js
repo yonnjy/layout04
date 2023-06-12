53e3b1584fb891d683be62032d29f2ec9b831cd4
YoonJY <133613780+yonnjy@users.noreply.github.com>
2023-06-12 16:15:32 +0900

    230612
    230612

---
+++ js/casia.js
@@ -0,0 +1,26 @@
+$(function () {
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+
+        if (sct > 0) {
+            $('.header').addClass('on')
+        } else {
+            $('.header').removeClass('on')
+        }
+    });
+
+    $('.mainSlide').slick({
+        fade: true,
+        dots: true,
+        autoplay: true,
+        arrows: false,
+    }
+    );
+
+    $('.mainVisual .arrows .left').on('click', function () {
+        $('.mainSlide').slick('slickPrev');
+    });
+    $('.mainVisual .arrows .right').on('click', function () {
+        $('.mainSlide').slick('slickNext');
+    });
+});
